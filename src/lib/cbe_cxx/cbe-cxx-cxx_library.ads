688dddb09792a439620fcbec180ddbf1651723c6
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-07-14 17:13:46 +0200

    fixup "cbe: front end for getting cbe device information" (cbe_cxx*)

--- src/lib/cbe_cxx/cbe-cxx-cxx_library.ads
+++ src/lib/cbe_cxx/cbe-cxx-cxx_library.ads
@@ -34,6 +34,14 @@ is
       Convention    => C,
       External_Name => "_ZN3Cbe7LibraryC2Ev";
 
+   procedure Current_Info (
+      Obj  :     Library.Object_Type;
+      Info : out CXX_Info_Type)
+   with
+      Export,
+      Convention    => C,
+      External_Name => "_ZNK3Cbe7Library13_current_infoERNS_4InfoE";
+
    procedure Active_Snapshot_IDs (
       Obj :     Library.Object_Type;
       IDs : out Active_Snapshot_IDs_Type)
